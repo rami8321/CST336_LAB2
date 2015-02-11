c1ec9aa0f6f3fc27fd5df1b5d2eda12a1c4c7fdf
tray9888 <mtrayford@csumb.edu>
2015-02-11 19:10:27 +0000

    style sheet and new index

--- index.php
+++ index.php
@@ -4,3 +4,24 @@
 
 ?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 2: Silverjack</title>
+        <meta charset="utf-8"/>
+        <link href="styles.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="wrapper">
+            <img src="img/miranda.png" alt="Miranda Trayford"/>
+            <br />
+            <img src="img/alonso.png" alt="Alonso Mendoza"/>
+            <br />
+            <img src="img/elias.png" alt="Elias Ramirez"/>
+            <br />
+            <img src="img/adrian.png" alt="Adrian Miguel"/>
+        </div>    
+    </body>
+</html>
+
